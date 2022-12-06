14f4aa37f2b5056294ca3a71f25730d48125ef43
Robert <109656109+RobertTwardowski@users.noreply.github.com>
2022-12-06 13:36:32 +0100

    Finish

--- script.js
+++ script.js
@@ -2,10 +2,16 @@
 const games = document.querySelector(".games");
 const grid = document.querySelector(".grid");
 const main = document.querySelector("#main");
-const contact = document.querySelector("#contact");
 const contactBtn = document.querySelector(".contact");
 const about = document.querySelector('#about')
 const aboutBtn = document.querySelector('.about')
+const btn = document.querySelector('.btn1')
+const btnSecond = document.querySelector('.btn2')
+const contactMessage = document.querySelector('#contact-message')
+const inputs = document.querySelectorAll('.input')
+const message = document.querySelector('#message')
+const finishMessage = document.querySelector('.finish-form')
+const btnThird = document.querySelector('.btn3')
 
 const addAnimationOnGames = () => {
   grid.classList.remove("hidden");
@@ -18,12 +24,11 @@ const addAnimationOnGames = () => {
 const mainHidden = () => main.classList.add("hidden");
 const gridHidden = () => grid.classList.add('hidden');
 const aboutHidden = () => about.classList.add('hidden');
-const contactHidden = () => contact.classList.add('hidden');
+const contactHidden = () => contactMessage.classList.add('hidden')
 
 const contactAdd = () => {
-  
-  contact.classList.remove("hidden");
-  contact.classList.add("one");
+  message.classList.remove('hidden')
+  contactMessage.classList.remove("hidden");
   mainHidden()
   aboutHidden()
   gridHidden()
@@ -31,13 +36,27 @@ const contactAdd = () => {
 
 const aboutAdd = () => {
     about.classList.remove('hidden')
-    about.classList.add('one')
     mainHidden()
     contactHidden()
     gridHidden()
 
 }
+const sendMessage = () =>{
+inputs.forEach((input)=>{
+input.value = ''
+})
+message.classList.add('hidden')
+finishMessage.classList.remove('hidden')
+}
+const sendFinish = () => {
+  main.classList.remove('hidden')
+  finishMessage.classList.add('hidden')
+  contactMessage.classList.add('hidden')
+}
 
 games.addEventListener("click", addAnimationOnGames);
 contactBtn.addEventListener("click", contactAdd);
 aboutBtn.addEventListener('click',aboutAdd)
+btn.addEventListener('click',contactAdd)
+btnSecond.addEventListener('click',sendMessage)
+btnThird.addEventListener('click',sendFinish)
